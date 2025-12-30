14caa121eed232ed9874d85d17b33a83452ac71f
Abdulraheem Akinola <138920589+Raheemzee@users.noreply.github.com>
2025-12-31 00:38:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -185,4 +185,5 @@ def uploaded_file(filename):
 # ------------------ Run ------------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
